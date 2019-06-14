c7377c44511964460427766c205ae80ec21a6cbc
Brandon Green <jailhousejordans@gmail.com>
2019-06-14 13:03:59 -0500

    fixed functions, hit mvp, I believe

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,16 +7,22 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function consume (a, b, cb) {
-  return  (cb(a, b));
+const consume = function(a, b, cb) {
+  return( cb(a, b));
 }
-console.log(cb(a, b))
+
+
+
+
+
+//console.log(consume(a, b))
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-const add = function (a, b) {
+
+function add (a, b) {
   return a + b;
 }
 
@@ -28,9 +34,9 @@ function greeting(first, last){
   return `Hello ${first} + ${last}, nice to meet you!`;
 }
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -43,13 +49,13 @@ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 // const external = "I'm outside the function";
 
-// function myFunction() {
-//   console.log(external);
-//   const internal = "Hello! I'm inside myFunction!";
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
 
-//   function nestedFunction() {
-//     console.log(internal);
-//   };
-//   nestedFunction();
-// }
-// myFunction();
+  function nestedFunction() {
+    console.log(internal);
+  };
+  nestedFunction();
+}
+myFunction();
